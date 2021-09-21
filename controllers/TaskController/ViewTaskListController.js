9748e794934e58ed5a1cc85322544b92d7905d70
Stepan Osadchii <stepanosadchii103@gmail.com>
2021-09-22 01:17:39 +0700

    New Task List Controller

--- controllers/TaskController/ViewTaskListController.js
+++ controllers/TaskController/ViewTaskListController.js
@@ -4,11 +4,18 @@ class ViewTaskListController {
     ViewList(req, res) {
         let task = new Task();
         task.findAll({
-            where: {'user_ id': req.body.user_id},
+            where: {'user_ id': req.body.id},
             raw : false,
         }).then(tasks => {
             res.send(
-                tasks.map(function (task){return task.date.getDay()}),
+                { tasks : tasks.map(function (task){return {
+                    task_id : task.id,
+                    date : task.date.getFullYear()+'-' + (task.date.getMonth()+1) + '-'+task.date.getDate()+' '+task.date.getHours()+':'+task.date.getMinutes(),
+                    status : task.status,
+                    group : task.group,
+                    info : task.info,
+                    deadline : task.deadline.getFullYear()+'-' + (task.deadline.getMonth()+1) + '-'+task.deadline.getDate()+' '+task.deadline.getHours()+':'+task.date.getMinutes()
+                }})},
             );
         })
 
