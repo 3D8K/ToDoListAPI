b5e3e41d2b1db6c9dcd773b52758f7b3a598ab20
Stepan Osadchii <stepanosadchii103@gmail.com>
2021-09-23 12:43:31 +0700

    Change old controllers

--- controllers/TaskController/ChangeTaskController.js
+++ controllers/TaskController/ChangeTaskController.js
@@ -3,7 +3,11 @@ const {Task} = require("../../models/Task")
 class ChangeTaskController{
     ChangeTask(req,res){
         let task = new Task();
-        task.update({info : req.body.info, deadline : req.body.deadline, group : req.body.group}, {where: {id : req.body.task_id, 'user_ id': req.body.user_id}});
+        task.update({info : req.body.info }, {where: {id : req.body.id, 'user_ id': req.body.user_id}}).then(task=>{
+            res.send({
+                success : true
+            })
+        });
     }
 }
 
--- controllers/TaskController/CompleteTaskController.js
+++ controllers/TaskController/CompleteTaskController.js
@@ -3,7 +3,7 @@ const {Task} = require("../../models/Task")
 class CompleteTaskController {
     completeTask(req, res) {
         let task = new Task();
-        task.update({status: false}, {where: {id: req.body.id, 'user_ id': req.body.user_id}}).then(task => {
+        task.update({status: 0}, {where: {id: req.body.id, 'user_ id': req.body.user_id}}).then(task => {
             res.send({
                 success: true,
             });
